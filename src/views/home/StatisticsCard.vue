0ea839683f2cf23a87c10531cb72bf72abdfb382
Ahmed alkhatim Awadeljeed <alkhatim40@gmail.com>
2024-01-08 07:36:47 +0200

    widgits

--- src/views/home/StatisticsCard.vue
+++ src/views/home/StatisticsCard.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <div class = "bg-white rounded-lg shadow-lg p-6 flex justify-between items-center">
+        <div class = "bg-white rounded-lg shadow-sm p-6 flex justify-between items-center">
             <div class = "space-y-1">
-                <p class = "font-bold text-[18px]">{{ value }}</p>
+                <p class = "font-bold text-[17px]">{{ value }}</p>
                 <p class = "text-[14px] text-gray-400">{{ title }}</p>
             </div>
-            <div class="w-[50px] h-[50px] rounded flex justify-between items-center" :class = "['text-[' +  color + ']']">
-                1
+            <div class="w-[40px] h-[40px] rounded-full flex justify-center items-center" :class = "colors.bg[color]">
+                <slot :color = "colors.color[color]"></slot>
             </div>
         </div>
     </div>
@@ -15,6 +15,21 @@
 <script setup>
 import { Card } from "@/components"
 const props = defineProps(["color", "title", "value"])
+
+const colors = {
+    bg :  {
+        green : "bg-green-200",
+        primary : "bg-stone-200",
+        blue : "bg-blue-200",
+        red : "bg-red-200",
+    },
+    color : {
+        green : "rgb(74 222 128)",
+        primary : "rgb(168 162 158)",
+        blue : "rgb(96 165 250)",
+        red : "rgb(248 113 113)",
+    }
+}
 </script>
 
 <style lang="scss" scoped>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,24 @@
 <template>
     <div class = "grid grid-cols-4 gap-4">
-        <StatisticsCard title=" رصيد المحفظة الرئيسية" color="red" value="10000 ريال"/>
-        <StatisticsCard title="إجمالي المحافظ الفرعية" color="green" value="10000 ريال"/>
-        <StatisticsCard title="سائقين" color="primary" value="15000"/>
-        <StatisticsCard title="مركبات" color="error" value="10060 "/>
+        <StatisticsCard title=" رصيد المحفظة الرئيسية" color="green" value="10000 ريال" v-slot="{color}">
+            <IconWallet :color="color" size="24"/>
+        </StatisticsCard>
+        <StatisticsCard title="إجمالي المحافظ الفرعية" color="primary" value="10000 ريال" v-slot="{color}">
+            <IconCash :color="color" size="26"/>
+        </StatisticsCard>
+        <StatisticsCard title="سائقين" color="blue" value="15000" v-slot="{color}">
+            <IconRadar :color="color" size="26"/>
+        </StatisticsCard>
+        <StatisticsCard title="مركبات" color="red" value="10060 " v-slot="{ color }">
+            <IconCar :color="color" size="26"/>
+        </StatisticsCard>
     </div>
 </template>
 
 <script setup>
 import StatisticsCard from './StatisticsCard.vue';
+
+import { IconWallet, IconCash, IconCar, IconRadar} from '@tabler/icons-vue';
 </script>
 
 <style lang="scss" scoped>
