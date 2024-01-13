f4dfc9545966e04b8e953837a0e8d2fd353bdcb1
Ahmed alkhatim Awadeljeed <alkhatim40@gmail.com>
2024-01-13 07:34:12 +0200

    branch api class

---
+++ src/api/branch.js
@@ -0,0 +1,22 @@
+import { useFetcher, useGet, usePost } from "@/composables"
+
+class BranchApiClass extends EventTarget {
+
+    on( event, callback){
+        this.addEventListener(event, callback)
+    }
+
+    getBranches = async () => {
+        const { response, error, getData } = useFetcher(useGet)
+        await getData('./branches')
+        if(response.value && response.value.status.code == 200) {
+            this.dispatchEvent(new CustomEvent('branchesFetched', { detail : response.value.data}))
+        } else if (error.value) {
+            this.dispatchEvent(new CustomEvent('errorOnFetch', { detail : error.value }))
+        }
+    }
+
+}
+
+const BranchApi = new BranchApiClass()
+export default BranchApi
---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+import BranchApi from "./branch"
+
+export { BranchApi }
