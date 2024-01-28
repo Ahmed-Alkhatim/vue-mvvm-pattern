59b018c4a0e62e89876e959b361675f495c2f9cd
Ahmed alkhatim Awadeljeed <alkhatim40@gmail.com>
2024-01-28 13:37:03 +0200

    use Logging Status
    redirect user to /login when is not logged in
    block /login router when is logged in

--- src/composables/useLoggingStatus.js
+++ src/composables/useLoggingStatus.js
@@ -1,12 +1,13 @@
 import { onMounted } from "vue"
-
+import { useRouter } from "vue-router"
 export const useLoggingStatus = () => {
+    const router = useRouter()
     const isUserLoggedIn = localStorage.getItem('token')
     onMounted( () => {
         if (!isUserLoggedIn && window.location.pathname != "/login") {
-            window.location = "/login"
+            router.push("/login")
         } else if (isUserLoggedIn && window.location.pathname == "/login") {
-            window.location = "/"
+            router.push("/")
         }
     })
 } 
